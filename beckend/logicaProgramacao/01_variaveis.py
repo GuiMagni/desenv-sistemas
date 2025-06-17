4970341f06a3d8ab717e0eec9b4cf2a8ce7e7888
unknown <magniguilherme592@gmail.com>
2025-06-17 09:46:49 -0300

    trabalhando com variáveis

---
+++ beckend/logicaProgramacao/01_variaveis.py
@@ -0,0 +1,37 @@
+nome = "Ana"
+idade = 25
+altura = 1.68
+tem_carteira = True
+
+print("nome:", nome)
+print("Idade:", idade)
+print("Altura:", altura)
+print("Tem carteira?", tem_carteira)
+
+
+
+
+nome = "Maria"
+cidade = "São Paulo"
+
+
+idade = 30
+ano = 2025
+
+
+altura = 1.65
+peso = 60.50
+
+
+estudante = True
+empregado = False
+
+
+print("Nome:", nome)
+print("Cidade:", cidade)
+print("Idade:", idade)
+print("Ano:", ano)
+print("Altura:", altura)
+print("Peso:", peso)
+print("É estudante?", estudante)
+print("Está empregado?", empregado)
