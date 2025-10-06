9b9d99a4a98f4f337f4f40bd71e663c2c6cf20e6
Guilherme Magni <magniguilherme592@gmail.com>
2025-10-06 08:55:11 -0300

    msg

--- beckend/logicaProgramacao/explicacao_json.py
+++ beckend/logicaProgramacao/explicacao_json.py
@@ -12,12 +12,14 @@ except FileNotFoundError:
     print("Arquivo não encontrado. Um novo será criado.")
 
 # Coleta dados do usuário
-nome = input("Digite o nome do produto: ")
-quantidade = int(input("Digite a quantidade: "))
-preco = float(input("Digite o preço: "))
+id = int(input("Digite o id do Produto:"))
+nome = input("Digite o nome do produto:")
+quantidade = int(input("Digite a quantidade:"))
+preco = float(input("Digite o preço:"))
 
 # Cria o novo produto
 novo_produto = {
+    "id" : id,
     "nome": nome,
     "quantidade": quantidade,
     "preco": preco,
@@ -33,7 +35,37 @@ with open("loja.json", "w") as arquivo:
 
 print("Produto cadastrado com sucesso!")
 
+# Modificar Produto
 
+id_produto_modificar = int (input("Digite o id para modificar:"))
+nova_quantidade = int (input("Digite a nova quantidade:"))
+
+try:
+    with open ("loja.json","r") as arquivo:
+        inventario = json.load(arquivo)
+except FileNotFoundError:
+    print("arquivo não encontrado")
+
+produto_encontrado = False
+
+for produto in inventario:
+    if produto["id"] == id_produto_modificar:
+        # Colocamos a Nova Quantidade
+        produto["quantidade"] = nova_quantidade
+        produto["em_estoque"] = nova_quantidade > 0
+        produto_encontrado = True
+        break;
+
+if not produto_encontrado:
+    print("O produto com o id informado não foi encontrado")  
+
+else:
+    with open ("loja.json","w") as arquivo:
+        json.dump(inventario, arquivo , ident = 4) 
+
+    print("O arquivo foi alterado com sucesso!!")
+     
+        
 
 
 
